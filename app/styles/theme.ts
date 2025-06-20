a3d55955eb4a3e13e51fe9dfd66b9bc21d1a1f4d
Guilherme0364 <guilhermedg364@outlook.com>
2025-06-20 00:26:08 -0300

    feat: add theme

---
+++ app/styles/theme.ts
@@ -0,0 +1,25 @@
+const Theme = {
+  screen: {
+    backgroundColor: '#F9F9F9',
+    primaryColor: '#BBF246',
+    secondaryColor: '#BBF246',
+    titleColor: '#191D26',
+    textColor: '#000000',
+    buttonTextColor: '#FFFFFF',
+
+    activeIcon: '#384325',
+    inactiveIcon: '#384325',
+  },
+
+  components: {
+    inputBackground: '#FFFFFF',
+    inputBorder: '#ccc',
+    inputError: 'red',
+  },
+
+  typography: {
+    primaryFont: 'Poppins, sans-serif',
+  },
+};
+
+export { Theme };
